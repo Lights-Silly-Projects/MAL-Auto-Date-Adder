c133a0aea84c86e17753adb8816b98ce017b0bc5
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2023-09-04 23:18:13 +0200

    Couple fixes, gc, etc.

--- mal_dates/parse.py
+++ mal_dates/parse.py
@@ -1,3 +1,4 @@
+import gc
 import re
 from calendar import month_name
 from time import sleep
@@ -8,7 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.select import Select
-from tqdm import tqdm
+from tqdm import tqdm  # type:ignore[import]
 
 from .browse import wait
 from .env import DEBUG
@@ -28,116 +29,163 @@ def process_list(driver: Chrome, scroll_time: float = 3.0) -> Chrome:
 
     print(f"Collecting all the shows on your {list_type} list")
 
-    shows = tqdm(get_anime_list(driver, scroll_time))
+    shows = tqdm(get_anime_list(driver, scroll_time), desc="Starting up...")
     action = ActionChains(driver)
 
+    failed: list[str] = []
+
     for i, show in enumerate(shows):
-        shows.set_description("Finding name")
-
-        driver.execute_script("arguments[0].scrollIntoView(true);", show)
-        sleep(0.5)
-
-        title = show.find_element(By.CLASS_NAME, "title")
-        name = title.find_element(By.CLASS_NAME, "link").text.strip()
-
-        shows.set_description(f"Processing \"{name}\"")
-
-        ep_count = show.find_elements(By.XPATH, f"//*[starts-with(@class, 'progress-')]")[i]
-        ep_count = ep_count.text.replace("\n", "").strip()
-
-        force_no_finished = False
-
         try:
-            total_episodes = int(ep_count)
-        except (TypeError, ValueError):
-            if x := re.findall(r"([\d-]+)", ep_count):
-                try:
-                    total_episodes = int(x[-1])
-                except (TypeError, ValueError):
+            driver.execute_script("arguments[0].scrollIntoView(true);", show)
+            sleep(0.5)
+
+            title = show.find_element(By.CLASS_NAME, "title")
+            name = title.find_element(By.CLASS_NAME, "link").text.strip()
+
+            shows.set_description(f"Processing \"{name}\" (Finding elements)")
+
+            ep_count = show.find_elements(By.XPATH, f"//*[starts-with(@class, 'progress-')]")[i]
+            ep_count = ep_count.text.replace("\n", "").strip()
+
+            force_no_finished = False
+
+            try:
+                total_episodes = int(ep_count)
+            except (TypeError, ValueError):
+                if x := re.findall(r"([\d-]+)", ep_count):
+                    try:
+                        total_episodes = int(x[-1])
+                    except (TypeError, ValueError):
+                        force_no_finished = True
+                else:
                     force_no_finished = True
-            else:
-                force_no_finished = True
 
-        shows.set_description(f"Processing \"{name}\" (Episode count found: {total_episodes})")
+            shows.set_description(f"Processing \"{name}\" (Episode count found: {total_episodes})")
 
-        get_started = not bool(show.find_element(By.CLASS_NAME, "started").text)
-        get_finished = not bool(show.find_element(By.CLASS_NAME, "finished").text)
+            get_started = not bool(show.find_element(By.CLASS_NAME, "started").text)
+            get_finished = not bool(show.find_element(By.CLASS_NAME, "finished").text)
 
-        if force_no_finished:
-            get_finished = False
+            if force_no_finished:
+                get_finished = False
 
-        if not any([get_started, get_finished]):
-            continue
+            if not any([get_started, get_finished]):
+                continue
 
-        start_dates, finished_dates = (1, 1, 1970), (1, 1, 1970)
-        start_changed, finished_changed = False, False
+            start_dates, finished_dates = (1, 1, 1970), (1, 1, 1970)
+            start_changed, finished_changed = False, False
 
-        # Enter the editing page
-        shows.set_description(f"Processing \"{name}\" (Accessing quick-edit page)")
+            # Enter the editing page
+            shows.set_description(f"Processing \"{name}\" (Accessing quick-edit page)")
 
-        get_y = list(show.find_element(By.CLASS_NAME, 'List_LightBox').location.values())[1]
+            get_y = list(show.find_element(By.CLASS_NAME, 'List_LightBox').location.values())[1]
 
-        driver.execute_script(f"window.scrollTo(0, {get_y - min(int(46 - i / 4), 54)});")
+            # This is to offset the header without going too far that the bottom entries can't be parsed.
+            driver.execute_script(f"window.scrollTo(0, {get_y - min(int(46 + i / 4), 54)});")
 
-        show.find_element(By.CLASS_NAME, "List_LightBox").click()
-        wait(driver, By.ID, "fancybox-outer", 3)
+            show.find_element(By.CLASS_NAME, "List_LightBox").click()
+            wait(driver, By.ID, "fancybox-outer", 3)
 
-        driver.switch_to.frame("fancybox-frame")
+            driver.switch_to.frame("fancybox-frame")
 
-        # Enter the history page
-        shows.set_description(f"Processing \"{name}\" (Checking history)")
-        wait(driver, By.LINK_TEXT, "History")
-        driver.find_element(By.LINK_TEXT, "History").click()
+            # Enter the history page
+            shows.set_description(f"Processing \"{name}\" (Checking history)")
+            wait(driver, By.LINK_TEXT, "History")
 
-        wait(driver, By.CLASS_NAME, "spaceit_pad")
-        watch_dates = driver.find_elements(By.CLASS_NAME, "spaceit_pad")
+            driver.find_element(By.LINK_TEXT, "History").click()
 
-        shows.set_description(f"Processing \"{name}\" (Checking history ({len(watch_dates)} dates found))")
+            wait(driver, By.CLASS_NAME, "spaceit_pad")
+            watch_dates = driver.find_elements(By.CLASS_NAME, "spaceit_pad")
 
-        watch_dates.reverse()
+            shows.set_description(f"Processing \"{name}\" (Checking history ({len(watch_dates)} dates found))")
 
-        if get_started:
-            start_dates = find_date(watch_dates[0].text)
+            watch_dates.reverse()
 
-        if get_finished:
-            for watch_date in watch_dates:
-                if not _check_total_episodes_matches(watch_date, total_episodes):
-                    continue
+            if get_started:
+                start_dates = find_date(watch_dates[0].text)
 
-                finished_dates = find_date(watch_date.text)
-                break
+            if get_finished:
+                for watch_date in watch_dates:
+                    if not _check_total_episodes_matches(watch_date, total_episodes):
+                        continue
 
-        # Back to the editing page
-        shows.set_description(f"Processing \"{name}\" (Editing dates)")
-        driver.back()
+                    finished_dates = find_date(watch_date.text)
+                    break
 
-        driver.switch_to.frame("fancybox-frame")
-        wait(driver, By.LINK_TEXT, "History")
+            # Back to the editing page
+            shows.set_description(f"Processing \"{name}\" (Editing dates)")
+            driver.back()
 
-        if get_started and start_dates[-1] != 1970:
-            driver = _pick_dropdowns(driver, start=True, dates=start_dates)
+            driver.switch_to.frame("fancybox-frame")
+            wait(driver, By.LINK_TEXT, "History")
 
-            start_changed = True
+            if get_started and start_dates[-1] != 1970:
+                driver = _pick_dropdowns(driver, start=True, dates=start_dates)
 
-        if get_finished and finished_dates[-1] != 1970:
-            driver = _pick_dropdowns(driver, start=False, dates=finished_dates)
+                start_changed = True
 
-            finished_changed = True
+            if get_finished and finished_dates[-1] != 1970:
+                driver = _pick_dropdowns(driver, start=False, dates=finished_dates)
 
-        if start_changed or finished_changed:
-            driver.find_elements(By.CLASS_NAME, "main_submit")[1].click()
+                finished_changed = True
 
-            wait(driver, By.CLASS_NAME, "goodresult")
+            if start_changed or finished_changed:
+                driver.find_elements(By.CLASS_NAME, "main_submit")[1].click()
 
-        shows.set_description(f"Processing \"{name}\" (Done)")
+                wait(driver, By.CLASS_NAME, "goodresult")
 
-        driver.execute_script(f"el = document.elementFromPoint(0, 0); el.click();")
-        # action.move_by_offset(-600, 0)
-        action.click().perform()
+            shows.set_description(f"Processing \"{name}\" (Done)")
 
-        driver.switch_to.default_content()
+            driver.execute_script(f"el = document.elementFromPoint(0, 0); el.click();")
+            action.click().perform()
 
-        wait(driver, By.CLASS_NAME, "watching")
+            driver.switch_to.default_content()
+
+            wait(driver, By.CLASS_NAME, "watching")
+        except TimeoutError as e:
+            shows.set_description("")
+            failed += [name]
+
+            print(f"Processing \"{name}\" (Failed! History for this entry is missing! Please fix it manually!)")
+
+            shows.set_description(f"Processing \"{name}\" (Failed!)")
+
+            driver.execute_script(f"el = document.elementFromPoint(0, 0); el.click();")
+            action.click().perform()
+        except BaseException as e:
+            shows.set_description(f"")
+            failed += [name]
+
+            print(f"Processing \"{name}\" (Failed! Please fix this entry manually!): {str(e)}")
+
+            shows.set_description(f"Processing \"{name}\" (Failed!)")
+
+            driver.execute_script(f"el = document.elementFromPoint(0, 0); el.click();")
+            action.click().perform()
+        finally:
+            if (i > 0) and (i % 50 == 0):
+                shows.set_description(f"Processing \"{name}\" (Clearing up memory)")
+
+                # Deleting a fuckton of vars
+                del title  # type:ignore[has-type]
+                del name  # type:ignore[has-type]
+                del get_started  # type:ignore[has-type]
+                del get_finished  # type:ignore[has-type]
+
+                # These vars have a chance of being skipped entirely every 50 entries
+                try:
+                    del start_dates  # type:ignore[has-type]
+                    del finished_dates  # type:ignore[has-type]
+                    del get_y  # type:ignore[has-type]
+                    del force_no_finished  # type:ignore[has-type]
+                    del watch_dates  # type:ignore[has-type]
+                except UnboundLocalError:
+                    pass
+
+                gc.collect()
+                sleep(8)
+
+    if failed:
+        print("\n - ".join([f"The following {len(failed)} shows could not be edited!"] + failed))
 
     return driver
 
